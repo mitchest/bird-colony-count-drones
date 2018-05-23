8329ab2c358ae216c353bcd5a77768ff2fa7b4db
KITE\Mitch <mitchell.lyons@unsw.edu.au>
2018-05-23 16:06:27 +1000

    add earth engine javascript

---
+++ earth_engine_code/blockb_rf.js
@@ -0,0 +1,186 @@
+// Booligal - lachlan river, block bank
+// Table of Contents
+
+// 0. Constants
+// 1. Loading the data
+// 2. Calculate features
+// 3. Train classifier and fit model
+// 4. Export
+
+// 0. Constants
+var EXPORT = true
+var NUM_TREES = 500
+var SEED = 42
+var NUM_SITES = 5 // out of 23
+var NON_NEST_POINTS = 1e4
+var REDUCER_SCALE = 0.05
+var EXPORT_SCALE = 0.1
+var USE_RGB = false
+
+var filename = function (){
+  return '/users/mitchest/drone-nest-count/blockb_rf_rscale' + 
+     REDUCER_SCALE*100 + '_exp_scale' + EXPORT_SCALE*100 + '_sites' + NUM_SITES
+}
+var OUTPUT_FILE = filename()
+
+// 1. Loading the data
+// load image data
+var ortho = ee.Image("users/mitchest/drone-nest-count/booligal_blockbank_ortho")
+// dsm's are a good choice if features of interest are distinct, in this case there's too much lignum that 'looks' like nests
+// var dsm = ee.Image("users/mitchest/drone-nest-count/")
+
+// load nest and other feature data
+var nests = ee.FeatureCollection('ft:15ajK6Jpd5dBj1SzCn2mzjAAR9mlkRFkUiGMLyk92') //identified nests
+var colony = ee.FeatureCollection("users/mitchest/drone-nest-count/blockb_colony_boundary") //colony boundary
+
+var training_sites = ee.FeatureCollection("users/mitchest/drone-nest-count/blockb_training_sites") //potnetial image training sites
+training_sites = training_sites.limit(NUM_SITES)
+
+// choose training data
+var buff_points = function (feature) {
+  return feature.buffer(30)
+}
+
+var training_buffers = training_sites.map(buff_points)
+var nests_training = nests.filterBounds(training_buffers)
+
+Map.addLayer({ eeObject: ortho, name: 'ortho' })
+Map.addLayer({ eeObject: training_buffers, name: 'training areas', shown: false })
+Map.addLayer({ eeObject: nests, name: 'all nests' })
+Map.addLayer({ eeObject: nests_training, name: 'training nests', shown: false })
+if(!EXPORT) Map.centerObject(nests_training, 24)
+
+// 2. Calculating features
+var calculate_predictors = function () {
+  // Create a more normalised brightness image
+  var white = ortho.select('b1').add(ortho.select('b2').divide(ortho.select('b3'))).rename('white')
+  // Local image 'topography' through laplacian kernel
+  var white_lapl8 = white.convolve(ee.Kernel.laplacian8())
+
+  // metrics to try distinguish vegetation (awesome if you fly a multispec sensor)
+  var grvi = ortho.expression(
+    '(g - r) / (g + r)', {
+      'g': ortho.select('b2'),
+      'r': ortho.select('b1')
+    }).rename('grvi')
+
+  var gbri = ortho.expression(
+    'g / (b + g + r)', {
+      'b': ortho.select('b3'),
+      'g': ortho.select('b2'),
+      'r': ortho.select('b1')
+    }).rename('gbri')
+
+  // glcm metrics on the ortho image (can choose when to do on normalised image and/or ortho data)
+  var white_glcm = white.toUint16().glcmTexture({ size: 1 }).select(1, 3, 4, 16)
+  var blue_glcm = ortho.select('b3').glcmTexture({ size: 1 }).select(16)
+
+  var boxcar = ee.Kernel.square({
+    radius: 5, units: 'pixels', normalize: true
+  })
+
+  var blue_glcm_smooth = blue_glcm.convolve(boxcar).rename('blue_glcm')
+
+  // add trianing data at this point together
+  var train_composite = white.addBands([white_lapl8, grvi, gbri, white_glcm, blue_glcm_smooth])
+  if(USE_RGB) train_composite = train_composite.addBands(ortho)
+  
+  // This feature is about the stdDeviation in a circle around the point
+  train_composite = train_composite.addBands([2, 7].map(
+    function (r) {
+      return train_composite.select(['gbri', 'white_shade']).reduceNeighborhood({
+        reducer: ee.Reducer.stdDev(),
+        kernel: ee.Kernel.circle(r)
+      })// .rename('circle_r_' + r)
+    }))
+  print(train_composite.bandNames())
+  // try a difference of gaussians (dog) edge detection kernel on the white ortho image
+  // this is the kernel
+  var dog = ee.Kernel.gaussian(4, 3, "pixels", false).add(ee.Kernel.gaussian(3, 0.5, "pixels", false, -1))
+  // apply
+  var dog_1 = train_composite.select('white_shade_stdDev').convolve(dog).rename('dog1')
+  // iterate the dog
+  var dog_2 = dog_1.convolve(dog).rename('dog2')
+
+  return train_composite.addBands([dog_1, dog_2])
+}
+
+var train_composite = calculate_predictors()
+
+//train_composite = train_composite.select(['b1', 'b2', 'b3', 'white'])
+
+Map.addLayer(train_composite, {}, 'predictor layers')
+
+/*
+Map.addLayer(train_composite, {min: 0.32, max: 25783, bands: [
+  'white_shade_stdDev_1',
+  'white_shade_stdDev_1',
+  'gbri']},'stddev and gbri', false)
+*/
+
+/*
+There is lots of room for exploration here ie: potential for lots of (potentially really good) predictors.
+The predictors might also be really specific to the nest and local enviroment.
+So a supervised learning method that is able to do feature selection should be important. 
+*/
+
+
+// 3. Train classifier and fit model
+
+// boilerplate from now on
+var nests_class = ee.Image(1).toUint16().reduceToVectors({
+  geometry: nests_training,
+  geometryType: 'centroid',
+  scale: REDUCER_SCALE
+})
+
+// TODO check whether we need to make these points fall outside some distance from nests...
+// ANSWER: definitely yes...
+var non_nests = ee.Image(0).toUint16().reduceToVectors({
+  geometry: ee.FeatureCollection.randomPoints(colony, NON_NEST_POINTS, SEED),
+  geometryType: 'centroid',
+  scale: REDUCER_SCALE
+})
+
+
+var train_points = ee.FeatureCollection([nests_class, non_nests]).flatten()
+
+var my_bands = train_composite.bandNames()
+if(!EXPORT) print('train_image', my_bands)
+
+var train_vectors = train_composite.reduceRegions({
+  reducer: 'first',
+  collection: train_points,
+  scale: 0.1 // 4 cm around a point is probably enough (given nest marking point is variable)
+})
+
+var classifier = ee.Classifier.randomForest({
+  numberOfTrees: NUM_TREES,
+  minLeafPopulation: 10,
+  seed: SEED
+}).train({
+  features: train_vectors,
+  classProperty: 'label',
+  inputProperties: my_bands
+})
+
+var bird_nests = train_composite.classify(classifier)
+
+
+// 4. Export
+
+var export_img = function () {
+  Export.image.toAsset({
+    image: bird_nests,
+    description: 'blockb',
+    assetId: OUTPUT_FILE,
+    scale: EXPORT_SCALE,
+    region: colony,
+    maxPixels: 1e10
+  })
+}
+
+if (EXPORT) export_img() 
+else {
+  Map.addLayer(bird_nests.mask(bird_nests.eq(1)), { min: 0, max: 1 }, 'new')
+}
---
+++ earth_engine_code/eulimbah_rf.js
@@ -0,0 +1,201 @@
+// Lowbidgee - Eulimbah
+// Table of Contents
+
+// 0. Constants
+// 1. Loading the data
+// 2. Calculate features
+// 3. Train classifier and fit model
+// 4. Export
+
+// 0. Constants
+var EXPORT = true
+var NUM_TREES = 500
+var SEED = 42
+var NUM_SITES = 5 // out of 18
+var NON_NEST_POINTS = 1e4
+var REDUCER_SCALE = 0.05
+var EXPORT_SCALE = 0.1
+var USE_RGB = false
+
+var filename = function (){
+  return '/users/mitchest/drone-nest-count/eulimbah_rf_rscale' + 
+     REDUCER_SCALE*100 + '_exp_scale' + EXPORT_SCALE*100 + '_sites' + NUM_SITES
+}
+var OUTPUT_FILE = filename()
+
+// 1. Loading the data
+// load image data
+var ortho = ee.Image("users/mitchest/drone-nest-count/eulimbah_ortho")
+// dsm's are a good choice if features of interest are distinct, in this case there's too much lignum that 'looks' like nests
+// var dsm = ee.Image("users/mitchest/drone-nest-count/")
+
+// load nest and other feature data
+var nests = ee.FeatureCollection("ft:17r4vPjHR2_uv2Vv2dDfjlMyvykpAr6tJ4m_Jm_9i") //identified nests
+var colony = ee.FeatureCollection("users/mitchest/drone-nest-count/eulimbah_colony_east") //colony boundary
+
+var training_sites = ee.FeatureCollection("users/mitchest/drone-nest-count/eulimbah_training_sites_east") //potnetial image training sites
+training_sites = training_sites.limit(NUM_SITES)
+
+// choose training data
+var buff_points = function (feature) {
+  return feature.buffer(30)
+}
+
+var training_buffers = training_sites.map(buff_points)
+var nests_training = nests.filterBounds(training_buffers)
+
+// special case here for missing data - clip the colony boundary by the ortho data, and clip ortho data
+var ortho_colony = ortho.select(['b3']).gt(0).reduceToVectors({geometry: colony, scale: 0.1, maxPixels: 1e10})
+Map.addLayer(ortho_colony, {}, 'ortho colony clip')
+ortho = ortho.clip(ortho_colony)
+
+Map.addLayer({ eeObject: ortho, name: 'ortho' })
+Map.addLayer({ eeObject: training_buffers, name: 'training areas', shown: false })
+Map.addLayer({ eeObject: nests, name: 'all nests' })
+Map.addLayer({ eeObject: nests_training, name: 'training nests', shown: false })
+if(!EXPORT) Map.centerObject(nests_training, 24)
+
+
+// 2. Calculating features
+
+var calculate_predictors = function () {
+  // Create a more normalised brightness image
+  var white = ortho.select('b1').add(ortho.select('b2').divide(ortho.select('b3'))).rename('white')
+  // Local image 'topography' through laplacian kernel
+  var white_lapl8 = white.convolve(ee.Kernel.laplacian8())
+
+  // metrics to try distinguish vegetation (awesome if you fly a multispec sensor)
+  var grvi = ortho.expression(
+    '(g - r) / (g + r)', {
+      'g': ortho.select('b2'),
+      'r': ortho.select('b1')
+    }).rename('grvi')
+
+  var gbri = ortho.expression(
+    'g / (b + g + r)', {
+      'b': ortho.select('b3'),
+      'g': ortho.select('b2'),
+      'r': ortho.select('b1')
+    }).rename('gbri')
+
+  // glcm metrics on the ortho image (can choose when to do on normalised image and/or ortho data)
+  var white_glcm = white.toUint16().glcmTexture({ size: 1 }).select(1, 3, 4, 16)
+  var blue_glcm = ortho.select('b3').glcmTexture({ size: 1 }).select(16)
+
+  var boxcar = ee.Kernel.square({
+    radius: 5, units: 'pixels', normalize: true
+  })
+
+  var blue_glcm_smooth = blue_glcm.convolve(boxcar).rename('blue_glcm')
+
+  // add trianing data at this point together
+  var train_composite = white.addBands([white_lapl8, grvi, gbri, white_glcm, blue_glcm_smooth])
+  if(USE_RGB) train_composite = train_composite.addBands(ortho)
+  
+  // This feature is about the stdDeviation in a circle around the point
+  train_composite = train_composite.addBands([2, 7].map(
+    function (r) {
+      return train_composite.select(['gbri', 'white_shade']).reduceNeighborhood({
+        reducer: ee.Reducer.stdDev(),
+        kernel: ee.Kernel.circle(r)
+      })// .rename('circle_r_' + r)
+    }))
+  print(train_composite.bandNames())
+  // try a difference of gaussians (dog) edge detection kernel on the white ortho image
+  // this is the kernel
+  var dog = ee.Kernel.gaussian(4, 3, "pixels", false).add(ee.Kernel.gaussian(3, 0.5, "pixels", false, -1))
+  // apply
+  var dog_1 = train_composite.select('white_shade_stdDev').convolve(dog).rename('dog1')
+  // iterate the dog
+  var dog_2 = dog_1.convolve(dog).rename('dog2')
+
+  return train_composite.addBands([dog_1, dog_2])
+}
+
+var train_composite = calculate_predictors()
+
+//train_composite = train_composite.select(['b1', 'b2', 'b3', 'white'])
+
+Map.addLayer(train_composite, {}, 'predictor layers')
+
+/*
+Map.addLayer(train_composite, {min: 0.32, max: 25783, bands: [
+  'white_shade_stdDev_1',
+  'white_shade_stdDev_1',
+  'gbri']},'stddev and gbri', false)
+*/
+
+/*
+There is lots of room for exploration here ie: potential for lots of (potentially really good) predictors.
+The predictors might also be really specific to the nest and local enviroment.
+So a supervised learning method that is able to do feature selection should be important. 
+*/
+
+
+// 3. Train classifier and fit model
+
+// boilerplate from now on
+var nests_class = ee.Image(1).toUint16().reduceToVectors({
+  geometry: nests_training,
+  geometryType: 'centroid',
+  scale: REDUCER_SCALE
+})
+
+// TODO check whether we need to make these points fall outside some distance from nests...
+// ANSWER: definitely yes...
+var non_nests = ee.Image(0).toUint16().reduceToVectors({
+  geometry: ee.FeatureCollection.randomPoints(ortho_colony, NON_NEST_POINTS, SEED),
+  geometryType: 'centroid',
+  scale: REDUCER_SCALE
+})
+
+
+var train_points = ee.FeatureCollection([nests_class, non_nests]).flatten()
+//Map.addLayer({ eeObject: train_points, name: 'train points' })
+
+var my_bands = train_composite.bandNames()
+if(!EXPORT) print('train_image', my_bands)
+
+var train_vectors = train_composite.reduceRegions({
+  reducer: 'first',
+  collection: train_points,
+  scale: 0.1 // 10 cm around a point is probably enough (given nest marking point is variable)
+})
+// filter out any pesky points that still managed to slip in
+//print(train_vectors.size())
+train_vectors = train_vectors.filter(ee.Filter.neq('white', null))
+//Map.addLayer({ eeObject: train_vectors, name: 'train vectors' })
+//print(train_vectors.first())
+//print(train_vectors.filter(ee.Filter.eq("system:index", '1_+321031280-76918767')).first())
+//print(train_vectors.size())
+
+var classifier = ee.Classifier.randomForest({
+  numberOfTrees: NUM_TREES,
+  minLeafPopulation: 10,
+  seed: SEED
+}).train({
+  features: train_vectors,
+  classProperty: 'label',
+  inputProperties: my_bands
+})
+
+var bird_nests = train_composite.classify(classifier)
+
+
+// 4. Export
+
+var export_img = function () {
+  Export.image.toAsset({
+    image: bird_nests,
+    description: 'eulimbah_east',
+    assetId: OUTPUT_FILE,
+    scale: EXPORT_SCALE,
+    region: ortho_colony, // this needs to be updated too!
+    maxPixels: 1e10
+  })
+}
+
+if (EXPORT) export_img() 
+else {
+  Map.addLayer(bird_nests.mask(bird_nests.eq(1)), { min: 0, max: 1 }, 'new')
+}
