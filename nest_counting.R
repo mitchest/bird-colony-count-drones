671a7cdfca59bdd38968fb36985f2e4e05664690
KITE\Mitch <mitchell.lyons@unsw.edu.au>
2018-12-18 10:01:54 +1100

    add proper standard error to plot; add per-grid resampling plots

--- nest_counting.R
+++ nest_counting.R
@@ -14,14 +14,35 @@ source("nest_counting_functions.R")
 merrimajeel <- read_fishnet_data("nest_counts/fishnet_counts/merrimajeel_rf_data.csv", 
                                  area_thresh = 2400, crowd_thresh = 200, crown_bins = 8)
 blockb <- read_fishnet_data("nest_counts/fishnet_counts/blockb_rf_data.csv",
-                            area_thresh = 1500, crowd_thresh = 200, crown_bins = 4)
+                            area_thresh = 1500, crowd_thresh = 200, crown_bins = 8)
 maczoo <- read_fishnet_data("nest_counts/fishnet_counts/maczoo_rf_data.csv",
                             area_thresh = 2400, crowd_thresh = 1000, crown_bins = 8)
 eulimbah <- read_fishnet_data("nest_counts/fishnet_counts/eulimbah_rf_data.csv",
-                              area_thresh = 1500, crowd_thresh = 200, crown_bins = 4)
+                              area_thresh = 1000, crowd_thresh = 200, crown_bins = 8)
 
 
 
+# manual counting accuracy ------------------------------------------------
+
+# manually counted in-situ and via drone imagery
+merrimajeel_counts <- data.frame(ground = c(17,22,27,27,24,20,8,13,14,25,9,16),
+                                 drone = c(16,21,26,21,29,21,11,16,14,26,14,18))
+merrimajeel_interval <- get_interval(merrimajeel_counts$ground, merrimajeel_counts$drone)
+
+blockb_counts <- data.frame(ground = c(41,48,23,30,18,23,18,3),
+                                 drone = c(30,46,19,28,19,16,18,4))
+blockb_interval <- get_interval(blockb_counts$ground, merrimajeel_counts$drone)
+
+maczoo_counts <- data.frame(ground = c(8,26,11,20,2,12,30,19,8,8,10,12,26,14),
+                            drone = c(8,18,9,10,0,9,36,14,6,4,9,8,19,4))
+maczoo_interval <- get_interval(maczoo_counts$ground, merrimajeel_counts$drone)
+
+eulimbah_counts <- data.frame(ground = c(5,2,5,17,5,17,3,17,15,28,15,129),
+                            drone = c(6,4,5,22,4,15,4,17,6,17,16,116))
+eulimbah_interval <- get_interval(eulimbah_counts$ground, merrimajeel_counts$drone)
+
+
+                                
 
 # crunch the numbers for each colony --------------------------------------
 
@@ -29,41 +50,104 @@ eulimbah <- read_fishnet_data("nest_counts/fishnet_counts/eulimbah_rf_data.csv",
 # crunch_it <- function(run_iters = F, save = F, # whether to re-run analysis/save data or just load and plot
 #                       colony_name, colony_data, # name of the colony (string) and loaded colony data
 #                       nboot = 1000, # number of bootstrap iterations for nest count estimates
-#                       smaller_sample = 5, num_draws = 3, # number of samples to take from each crowd ratio bin (from data load), repeated num_draws times
+#                       smaller_sample = 5, num_draws = 3, # number of grids to sample, repeated num_draws times ##### XXX ##### number of samples to take from each crowd ratio bin (from data load), repeated num_draws times
+#                       quants = c(0.1,0.9), # central quantile to sample from (trim extreme grids you wouldn't choose to count) 
+#                       model_type = NULL, # choose 'area', 'glm' or 'gam'
 #                       kfold_iters = 50, kfold_k = 10, # kfold iters and k for simulation
 #                       error_level = 0.05){ # manual counting error level to compare on plots
 
+run_resampling <- FALSE # whether to run or just plot
+save_resampling <- FALSE # whether to save results to disk
+model_type <- 'area' # choose area, glm or gam to model estimates
+nboot <- 800
+num_draws <- 40
+plot_only <- T
+
 ## merrimajeel creek - lower lachlan river
-crunch_it(run_iters = T, save = T,
+merrimajeel_estimates <- 
+  crunch_it(run_iters = run_resampling, save = save_resampling,
           "merrimajeel", merrimajeel, 
-          nboot = 800, 
-          smaller_sample = 4, num_draws = 10,
-          kfold_iters = 20, kfold_k = 10,
-          error_level = 0.05)
+          nboot = nboot, 
+          smaller_sample = 30, num_draws = num_draws,
+          model_type = model_type,
+          quants = c(0.05,0.95),
+          kfold_iters = 10, kfold_k = 10,
+          error_level = merrimajeel_interval/2,
+          plot_only = plot_only,
+          title = "Merrimajeel")
 
 # block bank - lower lachlan river
-crunch_it(run_iters = T, save = T,
+blockb_estimates <- 
+  crunch_it(run_iters = run_resampling, save = save_resampling,
           "blockb", blockb, 
-          nboot = 800, 
-          smaller_sample = 3, num_draws = 10,
-          kfold_iters = 20, kfold_k = 3,
-          error_level = 0.05)
+          nboot = nboot, 
+          smaller_sample = 10, num_draws = num_draws,
+          model_type = model_type,
+          quants = c(0.1,0.9),
+          kfold_iters = 10, kfold_k = 10,
+          error_level = blockb_interval/2,
+          plot_only = plot_only,
+          title = "Block Bank")
 
-# zoo paddock - macquaries marches
-crunch_it(run_iters = T, save = T,
+# zoo paddock - macquaries marshes
+zoomac_estimates <- 
+  crunch_it(run_iters = run_resampling, save = save_resampling,
           "maczoo", maczoo, 
-          nboot = 800, 
-          smaller_sample = 4, num_draws = 10,
-          kfold_iters = 20, kfold_k = 10,
-          error_level = 0.05)
+          nboot = nboot, 
+          smaller_sample = 30, num_draws = num_draws,
+          model_type = model_type, 
+          quants = c(0.1,0.9),
+          kfold_iters = 10, kfold_k = 10,
+          error_level = maczoo_interval/2,
+          plot_only = plot_only,
+          title = "Zoo Paddock")
 
 # eulimbah - lowbidgee
-crunch_it(run_iters = T, save = T,
+eulimbah_estimates <- 
+  crunch_it(run_iters = run_resampling, save = save_resampling,
           "eulimbah", eulimbah, 
-          nboot = 800, 
-          smaller_sample = 3, num_draws = 3,
-          kfold_iters = 20, kfold_k = 3,
-          error_level = 0.05)
+          nboot = nboot, 
+          smaller_sample = 15, num_draws = num_draws,
+          model_type = model_type,
+          quants = c(0.1,0.9),
+          kfold_iters = 10, kfold_k = 10,
+          error_level = eulimbah_interval/2,
+          plot_only = plot_only,
+          title = "Eulimbah")
 
+
+
+# paper figures -----------------------------------------------------------
+
+library(gridExtra)
+
+## per-grid estimates figure (supp. fig.)
+grid.arrange(estimate_nests_kfold_grids(30, merrimajeel, 10, "Merrimajeel"),
+             estimate_nests_kfold_grids(15, blockb, 10, "Block Bank"),
+             estimate_nests_kfold_grids(30, maczoo, 10, "Zoo Paddock"),
+             estimate_nests_kfold_grids(10, eulimbah, 10, "Eulimbah"),
+             ncol = 2)
+
+
+## monte carlo resamplign estimates (supp. fig.)
+grid.arrange(merrimajeel_estimates[[1]], merrimajeel_estimates[[2]],
+             blockb_estimates[[1]], blockb_estimates[[2]],
+             zoomac_estimates[[1]], zoomac_estimates[[2]],
+             eulimbah_estimates[[1]], eulimbah_estimates[[2]],
+             ncol = 2)
+
+
+## k-fold figure
+merrim_kfold <- readRDS("merrimajeel_kfold_estimates.rds")
+blockb_kfold <- readRDS("blockb_kfold_estimates.rds")
+maczoo_kfold <- readRDS("maczoo_kfold_estimates.rds")
+eulimb_kfold <- readRDS("eulimbah_kfold_estimates.rds")
+
+merrim_plt <- plot_nest_estimates_kfold(merrim_kfold, merrimajeel, merrimajeel_interval/2, "Merrimajeel")
+blockb_plt <- plot_nest_estimates_kfold(blockb_kfold, blockb, blockb_interval/2, "Block Bank")
+maczoo_plt <- plot_nest_estimates_kfold(maczoo_kfold, maczoo, maczoo_interval/2, "Zoo Paddock")
+eulimb_plt <- plot_nest_estimates_kfold(eulimb_kfold, eulimbah, eulimbah_interval/2, "Eulimbah")
+
+grid.arrange(merrim_plt, blockb_plt, maczoo_plt, eulimb_plt, ncol = 2)
 
 
--- nest_counting_functions.R
+++ nest_counting_functions.R
@@ -11,6 +11,12 @@ read_fishnet_data <- function(path, area_thresh = 2400, crowd_thresh = 200, crow
            Id = 1:n())
 }
 
+get_interval <- function(true, count, z = 1.96) {
+  error <- abs(sum(true) - sum(count)) / sum(true)
+  print(paste0("Count error was: ", error))
+  z * sqrt( (error * (1 - error)) / length(true))
+}
+
 estimate_nest <- function(samples, data) {
   Ids <- sample(x = data$Id, size = samples)
   train_data <- filter(data, Id %in% Ids)
@@ -55,12 +61,31 @@ plot_nest_estimates <- function(estimates_long, data, count_error, title) {
     xlab("number random transects used") + ylab("estimated nest count") + 
     ggtitle(title) + theme_bw()
   print(plt)
+  invisible(plt)
 }
 
-fit_kfold_glm <- function(x, kfold_data, data) { ############### wait, don't predict out to all original data - just the data we don't have coutns for!
+fit_kfold_area <- function(x, kfold_data, data) { ############### wait, do we want to predict out to all original data, or just the data we don't have coutns for?
   kdat <- filter(kfold_data, fold != x)
-  nest_glm <- glm.nb(nest_count ~ nest_area + crowd_ratio, data = kdat)
+  sum(data$nest_area) / (sum(kdat$nest_area) / sum(kdat$nest_count))
+}
+
+fit_kfold_area_grids <- function(x, kfold_data, data) { 
+  kdat <- filter(kfold_data, fold != x)
+  data.frame(manual_counts = data$nest_count,
+             area_estimates = data$nest_area / (kdat$nest_area / kdat$nest_count))
+}
+
+fit_kfold_glm <- function(x, kfold_data, data) { ############### wait, do we want to predict out to all original data, or just the data we don't have coutns for?
+  kdat <- filter(kfold_data, fold != x)
+  nest_glm <- glm(nest_count ~ nest_area + crowd_ratio, family = "poisson", data = kdat)
   sum(predict(nest_glm, newdata = data.frame(nest_area = data$nest_area, crowd_ratio = data$crowd_ratio), type = "response"))
+}
+
+fit_kfold_glm_grids <- function(x, kfold_data, data) { ############### wait, do we want to predict out to all original data, or just the data we don't have coutns for?
+  kdat <- filter(kfold_data, fold != x)
+  nest_glm <- glm(nest_count ~ nest_area + crowd_ratio, family = "poisson", data = kdat)
+  data.frame(manual_counts = data$nest_count,
+             area_estimates = predict(nest_glm, newdata = data.frame(nest_area = data$nest_area, crowd_ratio = data$crowd_ratio), type = "response"))
 }
 
 fit_kfold_gam <- function(x, kfold_data, data) {
@@ -69,26 +94,85 @@ fit_kfold_gam <- function(x, kfold_data, data) {
   sum(predict(nest_gam, newdata = data.frame(nest_area = data$nest_area, crowd_ratio = data$crowd_ratio), type = "response"))
 }
 
-estimate_nests_kfold <- function(samples, data, iters, k, model = NULL) {
-  if (is.null(model)) {stop("specify 'glm or 'gam'")}
+estimate_nests_kfold <- function(samples, data, iters, k, model = NULL, quants = c(0.1,0.9)) {
+  if (!model %in% c('glm','gam','area')) {stop("specify 'glm, 'gam' or 'area'")}
   kfold_data <- data %>%
-    group_by(crowd_bin) %>% #stratify sample
-    sample_n(samples)
-  #sample(x = data$Id, size = samples) %>%
-  #kfold_data <- filter(data, Id %in% Ids)
-  kfold_data <- kfold_data %>%
-    group_by(crowd_bin) %>% # stratify sample
-    mutate(fold = sample(rep(sample(k), length.out = n())))
-  #kfold_data$fold <- sample(rep(1:k, length.out = nrow(kfold_data)))
+    # stratify by crowd bin
+    filter(crowd_ratio > quantile(crowd_ratio, quants[1]), crowd_ratio < quantile(crowd_ratio, quants[2])) %>%
+    sample_frac(1) %>%
+    group_by(crowd_bin) %>%
+    mutate(crowd_id = 1:n()) %>%
+    arrange(crowd_id) %>%
+    .[1:samples,] %>%
+    ungroup()
+    
+    # # SRS
+    # sample_n(samples)
+    
+    # # Sample quantile of crowd ratios
+    # filter(crowd_ratio > quantile(crowd_ratio, 0.10), crowd_ratio < quantile(crowd_ratio, 0.80)) %>%
+    # sample_n(samples)
+  
   if (model == 'glm') {
-    return(as.vector(replicate(n = iters,
-                        expr = {unlist(lapply(X = 1:k, FUN = fit_kfold_glm, kfold_data, data))})))
+    return(
+      as.vector(
+        replicate(n = iters,
+                  expr = {
+                    kfold_data <- kfold_data %>%
+                      #group_by(crowd_bin) %>% # stratify sample
+                      mutate(fold = sample(rep(sample(k), length.out = n())))
+                    unlist(lapply(X = 1:k, FUN = fit_kfold_glm, kfold_data, data))
+                  })))
   }
   if (model == 'gam') {
-    return(as.vector(replicate(n = iters,
-                        expr = {unlist(lapply(X = 1:k, FUN = fit_kfold_gam, kfold_data, data))})))
+    return(
+      as.vector(
+        replicate(n = iters,
+                  expr = {
+                    kfold_data <- kfold_data %>%
+                      #group_by(crowd_bin) %>% # stratify sample
+                      mutate(fold = sample(rep(sample(k), length.out = n())))
+                    unlist(lapply(X = 1:k, FUN = fit_kfold_gam, kfold_data, data))
+                  })))
+  }
+  if (model == 'area') {
+    return(
+      as.vector(
+        replicate(n = iters,
+                  expr = {
+                    kfold_data <- kfold_data %>%
+                      #group_by(crowd_bin) %>% # stratify sample
+                      mutate(fold = sample(rep(sample(k), length.out = n())))
+                    unlist(lapply(X = 1:k, FUN = fit_kfold_area, kfold_data, data))
+                  })))
   }
 }
+
+estimate_nests_kfold_grids <- function(samples, data, iters, title, k = 10, quants = c(0.1,0.9)) {
+  kfold_data <- data %>%
+    # stratify by crowd bin
+    filter(crowd_ratio > quantile(crowd_ratio, quants[1]), crowd_ratio < quantile(crowd_ratio, quants[2])) %>%
+    sample_frac(1) %>%
+    group_by(crowd_bin) %>%
+    mutate(crowd_id = 1:n()) %>%
+    arrange(crowd_id) %>%
+    .[1:samples,] %>%
+    ungroup()
+  grid_est <- bind_rows(replicate(n = iters,
+                                  expr = {
+                                    kfold_data <- kfold_data %>%
+                                    #group_by(crowd_bin) %>% # stratify sample
+                                    mutate(fold = sample(rep(sample(k), length.out = n())))
+                                    bind_rows(lapply(X = 1:k, FUN = fit_kfold_area_grids, kfold_data, data))},
+                                  simplify = F))
+  plt <- ggplot(grid_est, aes(manual_counts, area_estimates)) +
+    geom_point() +
+    ggtitle(title) + ylab("Manual grid count") + xlab("Semi-automated grid count") +
+    geom_abline(slope = 1, intercept = 0) +
+    theme_classic()
+  plt
+}
+
 
 plot_nest_estimates_kfold <- function(kfold_estimates, data, count_error, title) {
   tot_nests <- sum(data$nest_count)
@@ -96,55 +180,69 @@ plot_nest_estimates_kfold <- function(kfold_estimates, data, count_error, title)
   nests_upr <- tot_nests + tot_nests * count_error
   #plot data
   kfold_estimates_long <- kfold_estimates %>%
-    as.data.frame() %>%
+    as.data.frame(col.names = 1:length(kfold_estimates)) %>%
     gather() %>%
     # mutate(samples = as.numeric(unlist(lapply(key, function(x) strsplit(x, "_")[[1]][2]))),
     #        key = factor(key), samples = factor(samples))
-    mutate(key = factor(key))
+    mutate(key = factor(key)) %>%
+    group_by(key) %>%
+    summarise(mean = mean(value),
+              upr = quantile(value, 0.95),
+              lwr = quantile(value, 0.05)) %>%
+    ungroup() %>%
+    mutate(upr2 = upr - mean, lwr2 = mean - lwr)
+  #print(kfold_estimates_long)
   # plot
   options(scipen = 100000)
-  plt <- ggplot(kfold_estimates_long, aes(y = value)) + 
-    geom_boxplot(aes(x = key), outlier.shape = NA) +
-    scale_y_continuous(limits = quantile(kfold_estimates_long$value, c(0.01, 0.9))) +
+  plt <- ggplot(kfold_estimates_long, aes(x = key)) + 
+    #geom_boxplot(aes(x = key), outlier.shape = NA) +
+    geom_errorbar(aes(ymin = mean - lwr2, ymax = mean + upr2), width = 0, colour = "grey") +
+    geom_point(aes(y = mean), size = 2) +
+    scale_y_continuous(limits = c(min(kfold_estimates_long$lwr),max(kfold_estimates_long$upr))) +
     geom_hline(yintercept = c(tot_nests, nests_lwr, nests_upr), colour = 2) +
     #facet_wrap(~samples) +
-    xlab("Different random draws of training data") + ylab("Estimated nest count") + 
+    xlab("") +#xlab("Different random draws of training data") + 
+    ylab("Estimated nest count") + 
     ggtitle(title) + 
-    theme_bw() + theme(axis.text.x=element_blank())
+    theme_classic() + theme(axis.text.x=element_blank())
   print(plt)
 }
 
 crunch_it <- function(run_iters = F, save = F, # whether to re-run analysis/save data or just load and plot
                       colony_name, colony_data, # name of the colony (string) and loaded colony data
                       nboot = 1000, # number of bootstrap iterations for nest count estimates
-                      smaller_sample = 5, num_draws = 3, # number of samples to take from each crowd ratio bin (from data load), repeated num_draws times
+                      smaller_sample = 5, num_draws = 3, # number of grids to sample, repeated num_draws times ##### XXX ##### number of samples to take from each crowd ratio bin (from data load), repeated num_draws times
+                      quants = c(0.1,0.9), # central quantile to sample from (trim extreme grids you wouldn't choose to count) 
+                      model_type = NULL, # choose 'area', 'glm' or 'gam'
                       kfold_iters = 50, kfold_k = 10, # kfold iters and k for simulation
-                      error_level = 0.05){ # manual counting error level to compare on plots
+                      error_level = 0.05,
+                      plot_only = F,
+                      title = "Resampled estimates"){ # manual counting error level to compare on plots
   if (run_iters) {
     ## straight up area ratio
-    colony_estimates <- bind_cols(lapply(1:nrow(colony_data), estimate_nest_counts, nboot, colony_data))
-    colony_estimates_long <- gather(colony_estimates)
-    colony_estimates_long$samples <- as.numeric(unlist(lapply(colony_estimates_long$key, 
+    colony_estimates0 <- bind_cols(lapply(1:nrow(colony_data), estimate_nest_counts, nboot, colony_data))
+    colony_estimates <- gather(colony_estimates0)
+    colony_estimates$samples <- as.numeric(unlist(lapply(colony_estimates$key, 
                                                               function(x) strsplit(x, "_")[[1]][2])))
-    if (save) saveRDS(colony_estimates_long, file = paste0(colony_name,"_estimates.rds"))
-    rm(colony_estimates_long)
+    if (save) saveRDS(colony_estimates, file = paste0(colony_name,"_estimates.rds"))
+    rm(colony_estimates0)
     ## model nest area and crowdedness
-    colony_estimates_glm <- bind_cols(lapply(1:nrow(colony_data), estimate_nest_counts_glm, nboot, colony_data))
-    colony_estimates_glm_long <- gather(colony_estimates_glm)
-    colony_estimates_glm_long$samples <- as.numeric(unlist(lapply(colony_estimates_glm_long$key, 
+    colony_estimates_glm0 <- bind_cols(lapply(1:nrow(colony_data), estimate_nest_counts_glm, nboot, colony_data))
+    colony_estimates_glm <- gather(colony_estimates_glm0)
+    colony_estimates_glm$samples <- as.numeric(unlist(lapply(colony_estimates_glm$key, 
                                                                   function(x) strsplit(x, "_")[[1]][2])))
-    if (save) saveRDS(colony_estimates_glm_long, file = paste0(colony_name,"_estimates_glm.rds"))
-    rm(colony_estimates_glm_long)
+    if (save) saveRDS(colony_estimates_glm, file = paste0(colony_name,"_estimates_glm.rds"))
+    rm(colony_estimates_glm0)
     ## test out resampling with smaller sampling size
     kfold_samples <- rep(smaller_sample, num_draws)
-    samples_actual_n <- kfold_samples * length(unique(colony_data$crowd_bin))
+    samples_actual_n <- kfold_samples #* length(unique(colony_data$crowd_bin))
     # glm
-    colony_kfold_estimates_glm <- lapply(X = kfold_samples,
-                                         FUN = estimate_nests_kfold, colony_data, kfold_iters, kfold_k, 'glm')
-    attr(colony_kfold_estimates_glm, "sampling") <- paste0("k-fold: ", samples_actual_n, " out of ", nrow(colony_data), " plots")
-    #names(colony_kfold_estimates_glm) <- paste0(kfold_samples,"_",samples_actual_n)
-    if (save) saveRDS(colony_kfold_estimates_glm, file = paste0(colony_name,"_kfold_estimates_glm.rds"))
-    rm(colony_kfold_estimates_glm)
+    colony_kfold_estimates <- lapply(X = kfold_samples,
+                                         FUN = estimate_nests_kfold, colony_data, kfold_iters, kfold_k, model_type, quants)
+    attr(colony_kfold_estimates, "sampling") <- paste0("k-fold: ", samples_actual_n, " out of ", nrow(colony_data), " plots")
+    #names(colony_kfold_estimates) <- paste0(kfold_samples,"_",samples_actual_n)
+    if (save) saveRDS(colony_kfold_estimates, file = paste0(colony_name,"_kfold_estimates.rds"))
+    #rm(colony_kfold_estimates)
     # # gam
     # colony_kfold_estimates_gam <- lapply(X = kfold_samples,
     #                                      FUN = estimate_nests_kfold, colony_data, kfold_iters, kfold_k, 'gam')
@@ -152,13 +250,23 @@ crunch_it <- function(run_iters = F, save = F, # whether to re-run analysis/save
     # saveRDS(colony_kfold_estimates_gam, file = paste0(colony_name,"_kfold_estimates_gam.rds"))
     # rm(colony_kfold_estimates_gam)
   }
-  colony_estimates <- readRDS(paste0(colony_name,"_estimates.rds"))
-  colony_estimates_glm <- readRDS(paste0(colony_name,"_estimates_glm.rds"))
-  colony_kfold_estimates_glm <- readRDS(paste0(colony_name,"_kfold_estimates_glm.rds"))
-  #colony_kfold_estimates_gam <- readRDS(paste0(colony_name,"_kfold_estimates_gam.rds"))
   
-  plot_nest_estimates(colony_estimates, colony_data, error_level, "Area only estimate")
-  plot_nest_estimates(colony_estimates_glm, colony_data, error_level, "Area/crowd model")
-  plot_nest_estimates_kfold(colony_kfold_estimates_glm, colony_data, error_level, attr(colony_kfold_estimates_glm, "sampling"))
-  #plot_nest_estimates_kfold(colony_kfold_estimates_gam, colony_data, error_level, "GAM k-fold")
+  if (!run_iters) {
+    colony_estimates <- readRDS(paste0(colony_name,"_estimates.rds"))
+    colony_estimates_glm <- readRDS(paste0(colony_name,"_estimates_glm.rds"))
+    colony_kfold_estimates <- readRDS(paste0(colony_name,"_kfold_estimates.rds"))
+    #colony_kfold_estimates_gam <- readRDS(paste0(colony_name,"_kfold_estimates_gam.rds"))
+  }
+  
+  if (plot_only) {
+    area_plt <- plot_nest_estimates(colony_estimates, colony_data, error_level, paste0(title, " - area only"))
+    glm_plt <- plot_nest_estimates(colony_estimates_glm, colony_data, error_level, paste0(title, " - GLM"))
+    return(list(area_plt,glm_plt))
+  } else {
+    plot_nest_estimates(colony_estimates, colony_data, error_level, title)
+    plot_nest_estimates(colony_estimates_glm, colony_data, error_level, title)
+    plot_nest_estimates_kfold(colony_kfold_estimates, colony_data, error_level, attr(colony_kfold_estimates, "sampling"))
+    #plot_nest_estimates_kfold(colony_kfold_estimates_gam, colony_data, error_level, "GAM k-fold")
+    invisible(plot_nest_estimates_kfold)
+  }
 }
